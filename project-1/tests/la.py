3e08fba3cbcaf1604ac62602bfd127e04579dc64
SVBSH <getenv@protonmail.com>
2023-10-11 23:55:10 +0200

    update

--- project-1/tests/la.py
+++ project-1/tests/la.py
@@ -1,8 +1,7 @@
 from queue import PriorityQueue
 from typing import List, Set, Dict, Callable, Optional
 from math import sqrt
-import yaml
-
+from itertools import permutations
 
 GamePlan = List[int]
 
@@ -10,77 +9,121 @@ goal_state: GamePlan = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 initial_state: GamePlan = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 
+def get_side_length(game_plan: GamePlan) -> int:
+    game_plan_length = len(game_plan)
+    return int(sqrt(game_plan_length))
+
+
 def get_goal_state(game_plan_length: int) -> GamePlan:
     return [number for number in range(1, game_plan_length)] + [0]
 
 
 def heuristic_misplaced_positions(game_plan: GamePlan) -> int:
+    """
+        Counts the number of tiles that are not in their correct 
+        positions in the current state compared to the goal state. 
+        Returns: count of misplaced tiles in the game plan
+    """
     result = 0
     for index, number in enumerate(game_plan, start=1):
-        if (number == 0):
-            continue
-        if (number != index):
+        if (number != 0 and number != index):
             result += 1
     return result
 
 
-# Define a function to calculate the misplaced tiles heuristic
-def misplaced_tiles_heuristic(game_plan: GamePlan) -> int:
-    return sum([1 for i, j in zip(game_plan, goal_state) if i != j])
-
-
-
-def get_inversion_count(game_plan: GamePlan) -> int:
+def heuristic_manhattan_distance(game_plan: GamePlan) -> int:
     """
-        Calculates a count of inversions in the game plan
+        Returns: cum of Manhattan Distances between goal state and 
+        current state of the game plan.
+        Manhattan distance formula := |x1 - x2| + |y1 - y2|
+        https://en.wikipedia.org/wiki/Taxicab_geometry
     """
-    inversion_count = 0
-    for comp_index, comp_value in enumerate(game_plan):
-        if comp_value == 0:
+    game_plan_side_length: int = get_side_length(game_plan)
+    distance_sum = 0
+    for index, value in enumerate(game_plan):
+        if value == 0:
             continue
-        for index in range(comp_index, len(game_plan)):
-            if game_plan[index] != 0 and comp_value > game_plan[index]:
-                inversion_count += 1
-    return inversion_count
+        # get position axis of a current value 
+        current_position = (
+            index // game_plan_side_length, 
+            index % game_plan_side_length)
+        # get goal position axis of the current value 
+        goal_position = (
+            (value - 1) // game_plan_side_length,
+            (value - 1) % game_plan_side_length)
+        # calculate a distance between current and goal positions
+        axis_x_difference = current_position[0] - goal_position[0]
+        axis_y_difference = current_position[1] - goal_position[1]
+        distance = abs(axis_x_difference) + abs(axis_y_difference)
+        distance_sum += distance
+
+    return distance_sum
 
 
-def get_zero_position_row(game_plan: GamePlan, game_plan_side_length: int) -> int:
-    for index, number in enumerate(game_plan, start=0):
-        if number == 0:
-            return index // game_plan_side_length
-    return -1
-
-
-def is_solvable(game_plan: GamePlan, game_plan_side_length: int) -> bool:
-    inversion_count_end_state = 0 
-    zero_position_row = get_zero_position_row(game_plan, game_plan_side_length)
-    actual_inversion_count = get_inversion_count(game_plan)
-    return (inversion_count_end_state % 2) == (actual_inversion_count % 2) and (zero_position_row == (2 % 2))
+def is_solvable(game_plan: GamePlan) -> int:
+    inversions = 0
+    game_plan_length = len(game_plan)
+    # get total number of inversions
+    for i in range(game_plan_length):
+        for j in range(i + 1, game_plan_length):
+            if game_plan[i] > game_plan[j] and game_plan[i] != 0 and game_plan[j] != 0:
+                inversions += 1
+    return inversions % 2 == 0
 
 
 def test_is_solvable():
-    with open('solvable.yaml', 'r') as file:
-        parsed_file = yaml.safe_load(file)
-        index = 0
-        for combination in parsed_file:
-            index += 1
-            game_plan_length = len(combination)
-            game_plan_side_length = int(sqrt(game_plan_length))
-            if is_solvable(combination, game_plan_side_length):
-                # print(f'solvable: {combination}')
-                pass
-            else:
-                print(f'not solvable: {combination}')
-                exit(1)
-            # if (index == 3):
-            #     return
+    permutations_list = list(permutations(range(9)))
+    # Filter out the permutations that are not solvable
+    not_solvable = [p for p in permutations_list if is_solvable(p)]
+    # Convert the not solvable permutations to a list
+    not_solvable_list = [list(p) for p in not_solvable]
+    index = 0
+    for combination in not_solvable_list:
+        index += 1
+        if solve_puzzle(combination, heuristic_misplaced_positions) is None:
+            # print(f'not solvable: {combination}')
+            print('not solvable: ', combination)
+            print(index)
+            exit(1)
+            pass
+        else:
+            print(index)
+            # print(f'solvable: {combination}')
+            # exit(1)
+        continue
+        game_plan_length = len(combination)
+        game_plan_side_length = int(sqrt(game_plan_length))
+        if is_solvable(combination, game_plan_side_length):
+            # print(f'solvable: {combination}')
+            pass
+        else:
+            print(f'not solvable: {combination}')
+            exit(1)
+        # if (index == 3):
+        #     return
 
 
-# Implement BFS with heuristic and step tracking
+def get_neighbors_of_blank_tile(game_plan_side_length: int, empty_tile_index: int):
+    neighbors: List[int] = []
+    move_left: bool = empty_tile_index % game_plan_side_length > 0
+    move_right: bool = empty_tile_index % game_plan_side_length < game_plan_side_length - 1
+    move_up: bool = empty_tile_index // game_plan_side_length > 0
+    move_down: bool = empty_tile_index // game_plan_side_length < game_plan_side_length - 1
+    if move_left:
+        neighbors.append(empty_tile_index - 1)
+    if move_right:
+        neighbors.append(empty_tile_index + 1)
+    if move_up:
+        neighbors.append(empty_tile_index - game_plan_side_length)
+    if move_down:
+        neighbors.append(empty_tile_index + game_plan_side_length)
+    return neighbors
+
+
 def solve_puzzle(initial_state: GamePlan, heuristic: Callable[[GamePlan], int]) -> Optional[List[GamePlan]]:
     game_plan_length = len(initial_state)
     game_plan_side_length = int(sqrt(game_plan_length))
-    if not is_solvable(initial_state, game_plan_side_length):
+    if not is_solvable(initial_state):
         return None
     
     if len(initial_state) == 0:
@@ -90,86 +133,79 @@ def solve_puzzle(initial_state: GamePlan, heuristic: Callable[[GamePlan], int]) 
     goal_state: GamePlan = get_goal_state(game_plan_length)
     visited: Set[str] = set()
     queue = PriorityQueue()
-    steps: Dict[str, GamePlan] = {}  # Dictionary to store steps and their parent states
+    # store steps and their parent states
+    steps: Dict[str, GamePlan] = {}  
 
-    # Enqueue the initial state with its heuristic value and step 0
+    # initialize queue
     queue.put((initial_state, heuristic(initial_state), 0))
-    steps[str(initial_state)] = None  # Initial state has no parent
+    # initial state has no parent 
+    steps[str(initial_state)] = None  
     index = 0
     while not queue.empty():
         current_state, _, step = queue.get()
         
-
-        if current_state == goal_state:
-            # Solution found, backtrack steps
+        game_solved: bool = current_state == goal_state
+        if game_solved:
+            # reconstruct steps of the solution
             solution: List[GamePlan] = [current_state]
+
             while steps[str(current_state)] is not None:
                 current_state = steps[str(current_state)]
                 solution.append(current_state)
+
             solution.reverse()
             return solution
 
         visited.add(str(current_state))
+        empty_tile_index = current_state.index(0)
 
-        # Generate successor states by moving the empty tile
-        empty_position_index = current_state.index(0)
-        neighbors = []
-        if empty_position_index % game_plan_side_length > 0:
-            neighbors.append(empty_position_index - 1)  # Move left
-        if empty_position_index % game_plan_side_length < game_plan_side_length - 1:
-            neighbors.append(empty_position_index + 1)  # Move right
-        if empty_position_index // game_plan_side_length > 0:
-            neighbors.append(empty_position_index - game_plan_side_length)  # Move up
-        if empty_position_index // game_plan_side_length < game_plan_side_length - 1:
-            neighbors.append(empty_position_index + game_plan_side_length)  # Move down
+        # generate successor states by moving the empty tile
+        neighbors = get_neighbors_of_blank_tile(game_plan_side_length, empty_tile_index)
    
 
         for neighbor_index in neighbors:
             new_state: GamePlan = current_state.copy()
-            # move
-            new_state[empty_position_index], new_state[neighbor_index] = new_state[neighbor_index], new_state[empty_position_index]
+            # swap positions
+            new_state[empty_tile_index], new_state[neighbor_index] = \
+                new_state[neighbor_index], new_state[empty_tile_index]
 
             if str(new_state) not in visited:
                 queue.put((new_state, heuristic(new_state), step + 1))
-                # pp.pprint(queue.queue)
-                # print()
-                # if step == 3:
-                    # return
                 steps[str(new_state)] = current_state
-
-    return None  # No solution found
+    return None
 
 
 def print_game_plan(game_plan: GamePlan):
-    game_plan_length = len(initial_state)
+    game_plan_length = len(game_plan)
     game_plan_side_length = int(sqrt(game_plan_length))
     for row_index, value in enumerate(game_plan, start=1):
         sep = '\n' if row_index % game_plan_side_length == 0 else ' '
         print(f'{value:3}', end=sep)            
 
 
-# test_is_solvable()
-# Solve the puzzle
-# combination = [
-# 1,2,3,4,5,
-# 6,7,8,9,10,
-# 11,12,13,0,14,
-# 16,17,18,19,15,
-# 21,22,23,24,20
-# ]
-# solution_steps = solve_puzzle(combination, heuristic_misplaced_positions)
-# game_plan_length = len(combination)
-# game_plan_side_length = int(sqrt(game_plan_length))
+test_is_solvable()
 
-solution_steps = solve_puzzle(initial_state, heuristic_misplaced_positions)
-if solution_steps:
-    print("Solution found:")
-    for step, game_plan in enumerate(solution_steps, start=1):
-        print(f'-> Step: {step}')
-        print_game_plan(game_plan)
-        print()
-else:
-    print("No solution found.")
+# Solve the puzzle
+combination = [
+1,2,3,4,5,
+6,7,8,9,10,
+11,12,13,0,14,
+16,17,18,19,15,
+21,22,23,24,20
+]
+# solve_puzzle(combination, heuristic_manhattan_distance)
+# ss = is_solvable(combination, game_plan_side_length)
+# print(ss)
+
+# solution_steps = solve_puzzle(combination, heuristic_manhattan_distance)
+# if solution_steps:
+#     print("Solution found:")
+#     for step, game_plan in enumerate(solution_steps, start=1):
+#         print(f'-> Step: {step}')
+#         print_game_plan(game_plan)
+#         print()
+# else:
+#     print("No solution found.")
 
 
 # print(solve_puzzle(combination, heuristic_misplaced_positions)
