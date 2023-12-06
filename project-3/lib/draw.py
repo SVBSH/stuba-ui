ccb85d7ed8ab820b23195266a00caac5aef7569b
SVBSH <getenv@protonmail.com>
2023-12-06 21:23:41 +0100

    update

--- project-3/lib/draw.py
+++ project-3/lib/draw.py
@@ -48,13 +48,17 @@ def plot_clusters(coordinates, clusters):
 
 
 def plot_clusters_1(clusters):
-    """Plot the clusters with different colors."""
+    """Plot the clusters with different colors and add a label for each cluster."""
     colors = cm.rainbow(np.linspace(0, 1, len(clusters)))
-    for cluster, color in zip(clusters, colors):
+    for idx, (cluster, color) in enumerate(zip(clusters, colors)):
         cluster_points = np.array(cluster)
         plt.scatter(cluster_points[:, 0], cluster_points[:, 1], color=color)
+
+        # Compute the centroid of the cluster for labeling
+        centroid = np.mean(cluster_points, axis=0)
+        plt.text(centroid[0], centroid[1], f'Cluster {idx+1}', fontsize=12, ha='center', va='center')
+
     plt.xlabel('X Coordinate')
     plt.ylabel('Y Coordinate')
-    plt.title('Hierarchical Agglomerative Clustering')
+    plt.title('Hierarchical Agglomerative Clustering with Cluster Labels')
     plt.show()
-
--- project-3/m.py
+++ project-3/m.py
@@ -5,14 +5,28 @@ from typing import Tuple
 
 
 
-def adjust_range(value, space_range, offset_range):
-    """Adjust the range for offset to ensure the new point is within the space range."""
-    low, high = offset_range
-    if value + low < space_range[0]:
-        low = space_range[0] - value
-    if value + high > space_range[1]:
-        high = space_range[1] - value
-    return low, high
+# def adjust_range(value, space_range, offset_range):
+#     """Adjust the range for offset to ensure the new point is within the space range."""
+#     low, high = offset_range
+#     if value + low < space_range[0]:
+#         low = space_range[0] - value
+#     if value + high > space_range[1]:
+#         high = space_range[1] - value
+#     return low, high
+
+def adjust_range(base_point_axis: int, space_range: Tuple[int, int], offset_range: Tuple[int, int]):
+    min_offset = offset_range[0]
+    max_offset = offset_range[1]
+
+    min_diff = base_point_axis + offset_range[0]
+    if min_diff < space_range[0]:
+        min_offset = offset_range[0] + abs(abs(min_diff) - abs(space_range[0]))
+
+    max_diff = base_point_axis + offset_range[1]
+    if max_diff > space_range[1]:
+        max_offset = offset_range[1] - abs(abs(max_diff) - abs(space_range[1]))
+
+    return min_offset, max_offset
 
 def generate_random_points(num_points: int, 
                            space_range: Tuple[int, int], 
@@ -52,48 +66,68 @@ def manhattan_distance(point1, point2):
 def calculate_centroid(cluster):
     """Calculate the centroid of a given cluster using NumPy for efficiency."""
     return np.mean(cluster, axis=0)
+    
 
-def hac_optimized_with_heap(data, max_clusters):
-    """Optimized Hierarchical Agglomerative Clustering using a min heap for efficient distance retrieval."""
-    # Convert data to a NumPy array for efficient processing
+def hac_optimized_with_options(data, max_clusters, use_medoids=False):
+    """Optimized HAC with options to use centroids or medoids."""
     data = np.array(data)
     n_points = len(data)
     clusters = {i: [i] for i in range(n_points)}  # Dictionary to keep track of clusters
 
-    # Function to calculate the centroid of a cluster
+    # Function to calculate the centroid or medoid of a cluster
     def calculate_centroid(cluster_indices):
+        """Calculate the centroid of the cluster."""
         return np.mean(data[cluster_indices], axis=0)
 
-    # Create a min heap for distances
-    distance_heap = []
+    def calculate_medoid(cluster_indices):
+        """Calculate the medoid of the cluster."""
+        cluster_data = data[cluster_indices]
+        distance_matrix = np.sum(np.abs(cluster_data[:, np.newaxis] - cluster_data), axis=2)
+        medoid_index = np.argmin(np.sum(distance_matrix, axis=1))
+        return cluster_data[medoid_index]
+
+    # Choose the appropriate heuristic function
+    calculate_cluster_center = calculate_medoid if use_medoids else calculate_centroid
+
+
+    # Precompute all distances and store in a dictionary
+    distances = {}
     for i in range(n_points):
         for j in range(i+1, n_points):
             distance = manhattan_distance(data[i], data[j])
-            heapq.heappush(distance_heap, (distance, i, j))
+            distances[(i, j)] = distance
+
+    # Create a min heap for distances
+    distance_heap = [(dist, i, j) for (i, j), dist in distances.items()]
+    heapq.heapify(distance_heap)
 
     while len(clusters) > max_clusters:
         # Find the closest pair of clusters
         while True:
             distance, i, j = heapq.heappop(distance_heap)
             if i in clusters and j in clusters and i != j:
-                break
+                if distance == distances[(min(i, j), max(i, j))]:
+                    break
 
-        # Merge clusters
-        clusters[i].extend(clusters[j])
+        # Merge clusters and update distances
+        new_cluster = clusters[i] + clusters[j]
+        clusters[i] = new_cluster
         del clusters[j]
 
-        # Update distances in the heap for the merged cluster
+        # Update distances in the heap for the merged cluster using incremental updates
+        new_center = calculate_cluster_center(new_cluster)
         for k in clusters:
             if k != i:
-                new_distance = manhattan_distance(calculate_centroid(clusters[i]), calculate_centroid(clusters[k]))
-                heapq.heappush(distance_heap, (new_distance, i, k))
+                other_center = calculate_cluster_center(clusters[k])
+                new_distance = manhattan_distance(new_center, other_center)
+                distances[(min(i, k), max(i, k))] = new_distance
+                heapq.heappush(distance_heap, (new_distance, min(i, k), max(i, k)))
 
     # Prepare final clusters list
     final_clusters = [data[indices] for indices in clusters.values()]
     return final_clusters
-
 # Example data (feel free to replace with your data)
-data = [[2, 6], [3, 4], [3, 8], [4, 7], [6, 2], [7, 4], [8, 5], [9, 7]]
+coordinates = [[2, 6], [3, 4], [3, 8], [4, 7], [6, 2], [7, 4], [8, 5], [9, 7]]
 space_range = (-5000, 5000)
 initial_points = 20
 additional_points = 5000
@@ -101,10 +135,10 @@ offset_range = (-100, 100)
 coordinates = generate_random_points(initial_points, space_range, additional_points, offset_range)
 
 # Perform HAC with a maximum number of clusters
-max_clusters = 18  # For example, let's stop clustering when there are 3 clusters
+max_clusters = 12  # For example, let's stop clustering when there are 3 clusters
 
 # Use the previously defined hac_with_max_clusters function
-hac_clusters = hac_optimized_with_heap(coordinates, max_clusters)
+hac_clusters = hac_optimized_with_options(coordinates, max_clusters, use_medoids=True)
 
 # Plot the clusters
 plot_clusters_1(hac_clusters)
